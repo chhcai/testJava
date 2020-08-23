3fae40847b56a184100d724514c33cbea9b21084
CaiSt <chhcai@hotmail.com>
2020-08-23 12:02:41 -0400

    add feature1

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -3,6 +3,7 @@ package com.test;
 public class Test {
     public static void main(String[] args) {
         System.out.printf("test");
+        System.out.printf("feature1");
         System.out.printf("hotfix");
     }
 }
