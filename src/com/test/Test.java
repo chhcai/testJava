4919c73da7b867c8891ba83868ca0bafe87cf383
CaiSt <chhcai@hotmail.com>
2020-08-23 13:03:30 -0400

    master base

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -3,7 +3,5 @@ package com.test;
 public class Test {
     public static void main(String[] args) {
         System.out.printf("test");
-        System.out.printf("feature1");
-        System.out.printf("hotfix");
     }
 }
